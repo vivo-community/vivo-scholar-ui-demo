6fc42d6404ec2921ef18094fe8245545408a5f8d
Rob Nelson <robert.nelson@duke.edu>
2020-06-16 10:14:24 -0400

    remove non-existent specific tags in git clone batch files

--- download.bat
+++ download.bat
@@ -1,7 +1,11 @@
 @echo off
 REM note don't have Windows to check if this works
 
-git clone -b 1.0.0-RC1 https://github.com/vivo-community/scholars-discovery.git
-git clone -b 0.9.0 https://github.com/vivo-community/vivo-scholar-ui.git
+REM NOTE will change to something like this soon
+REM git clone -b 1.0.0-RC1 https://github.com/vivo-community/scholars-discovery.git
+REM git clone -b 0.9.0 https://github.com/vivo-community/vivo-scholar-ui.git
+
+git clone https://github.com/vivo-community/scholars-discovery.git
+git clone https://github.com/vivo-community/vivo-scholar-ui.git
 
 
--- download.sh
+++ download.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-git clone -b 1.0.0-RC1 https://github.com/vivo-community/scholars-discovery.git
-git clone -b 0.9.0 https://github.com/vivo-community/vivo-scholar-ui.git
+# NOTE: will change to something like this soon
+#git clone -b 1.0.0-RC1 https://github.com/vivo-community/scholars-discovery.git
+#git clone -b 0.9.0 https://github.com/vivo-community/vivo-scholar-ui.git
+
+git clone https://github.com/vivo-community/scholars-discovery.git
+git clone https://github.com/vivo-community/vivo-scholar-ui.git
 
 
