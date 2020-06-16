307377e9a31dd01f6a4b3e766f4c8157383c24db
Rob Nelson <robert.nelson@duke.edu>
2020-06-16 09:30:12 -0400

    update download files to point to tag

--- download.bat
+++ download.bat
@@ -1,6 +1,7 @@
 @echo off
 REM note don't have Windows to check if this works
 
-git clone https://github.com/vivo-community/scholars-discovery.git
-git clone https://github.com/vivo-community/vivo-scholar-ui.git
+git clone -b 1.0.0-RC1 https://github.com/vivo-community/scholars-discovery.git
+git clone -b 0.9.0 https://github.com/vivo-community/vivo-scholar-ui.git
+
 
--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone https://github.com/vivo-community/scholars-discovery.git
-git clone https://github.com/vivo-community/vivo-scholar-ui.git
+git clone -b 1.0.0-RC1 https://github.com/vivo-community/scholars-discovery.git
+git clone -b 0.9.0 https://github.com/vivo-community/vivo-scholar-ui.git
 
 
