c6ea3d03378c9522be6b627a03b3b96fde7eed35
Rob Nelson <robert.nelson@duke.edu>
2020-06-16 08:16:47 -0400

    try adding a windows batch file to download two repositories

---
+++ download.bat
@@ -0,0 +1,6 @@
+@echo off
+REM note don't have Windows to check if this works
+
+git clone https://github.com/vivo-community/scholars-discovery.git
+git clone https://github.com/vivo-community/vivo-scholar-ui.git
+
